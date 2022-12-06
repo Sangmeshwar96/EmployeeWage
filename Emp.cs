b156f4a986a5b7390881a615579e868191992e4f
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-06 10:25:19 +0530

    Multiple Companies Employee Wage Computation

---
+++ Emp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Emp
+    {
+        public int fullTime = 1;
+        int empHr = 0;
+        int empWage = 0;
+        public void Wage(string comp, int wagePerHr)
+        {
+            Random random = new Random();
+            int attendCheck = random.Next(0, 2);
+            if (attendCheck == fullTime)
+            {
+                empHr = 8;
+                Console.WriteLine($"The Employee is present");
+            }
+            else
+            {
+                empHr = 0;
+                Console.WriteLine($"The Employee wage is absent");
+            }
+            empWage = empHr * wagePerHr;
+            Console.WriteLine($"The Empployee wage is {empWage}");
+        }
+    }
+}
