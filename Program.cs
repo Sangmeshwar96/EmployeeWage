ecb51b071d0644852e9b147f935b8141651e18b5
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-06 10:27:41 +0530

    Writting a Class Method to Compute Employee Wage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EmployeeWage;
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---Welcome To Employee Wage program---");
+            Console.WriteLine("");
+            Emp emp = new Emp();
+            emp.Wage("TCS", 20);
+            emp.Wage("Infy", 30);
+        }
+    }
+}
