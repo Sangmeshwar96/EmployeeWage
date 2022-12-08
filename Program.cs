4025c10ec2dc20ea8f3080e22e85faf1c8acbacc
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-08 08:13:43 +0530

    Calculating Total EmployeeWage Of Each Company

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
 ﻿using System;
 using EmployeeWage;
-Console.WriteLine("---Welcome To Employee Wage program---");
-Console.WriteLine("");
-Emp emp = new Emp();
-emp.Wage();
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---Welcome To Employee Wage program---");
+            Console.WriteLine("");
+            Emp emp1 = new Emp();
+            emp1.Wage("TCS", 20, 20, 100);
+            Emp emp2 = new Emp();
+            emp2.Wage("Infy", 30, 20, 100);
+            Console.WriteLine("Total wage for an employee of TCS is : " + emp1.totalWage);
+            Console.WriteLine("Total wage for an employee of Infy is : " + emp2.totalWage);
+        }
+    }
+}
+
