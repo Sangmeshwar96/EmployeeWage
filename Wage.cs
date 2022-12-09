f9745961855acd70ed57f9b687ae28e256653c5a
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-09 18:39:09 +0530

    Getting the Total Wage when queried by Company

---
+++ Wage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeeWage
+{
+    public class Wages : IEmployeeWageForCompany
+    {
+        static int day_hr;
+        List<ParticularCompany> companyDetailsList;       
+        public Dictionary<string, ParticularCompany> companyDetailsDictionary;
+        public int ToMonWag = 0;                          
+        public Wages()
+        {
+            this.companyDetailsList = new List<ParticularCompany>();
+            this.companyDetailsDictionary = new Dictionary<String, ParticularCompany>();
+        }
+
+        public void AddDetails(String company, int WaPhr, int max_workDays, int max_workHrs)
+        {
+            ParticularCompany NewCompany = new ParticularCompany(company, WaPhr, max_workDays, max_workHrs);
+            companyDetailsList.Add(NewCompany);                                                          
+            companyDetailsDictionary.Add(company, NewCompany);
+        }
+
+        public void perCompany()           
+        {
+            foreach (var NewCompany in companyDetailsList)
+            {
+                Console.WriteLine(NewCompany.company + " - ");
+                NewCompany.TWage(compute(NewCompany));
+            }
+        }
+        public int wage(int WaPhr)
+        {
+            int ch = new Random().Next(0, 3);
+            day_hr = 0;
+            switch (ch)
+            {
+                case 1:
+                    day_hr = 4;
+                    break;
+                case 2:
+                    day_hr = 8;
+                    break;
+                default:
+                    day_hr = 0;
+                    break;
+            }
+           
+            return (WaPhr * day_hr);
+        }
+        public int compute(ParticularCompany Company)             
+        {
+            int days = 0;
+            int totHrs = 0;                
+
+            while (days < Company.max_workDays && totHrs < Company.max_workHrs)
+            {
+               
+                ToMonWag = ToMonWag + (wage(Company.WaPhr));
+                days++;
+                totHrs = totHrs + day_hr;
+                if (totHrs > Company.max_workHrs)                
+                {
+                    ToMonWag = ToMonWag - ((totHrs - Company.max_workHrs) * Company.WaPhr);
+                }
+            }
+            Console.WriteLine("Total Wage for the company is = " + ToMonWag);
+            return ToMonWag;
+        }
+        public void QuerrySolved(String CheckCompany)                             
+        {
+            int flag = 0;
+            foreach (var NewCompany in companyDetailsList)
+            {
+                if (((NewCompany.company).ToUpper()).Equals(CheckCompany.ToUpper()))
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("Total Wage for the employee for the company " + CheckCompany + " is Rs." + (companyDetailsDictionary[CheckCompany].totalWage));
+            }
+            else
+            {
+                Console.WriteLine("Company not in the storage data base");
+            }
+        }
+
+    }
+}
